588f04ffa00f927bb3605df6e1df275d6ca68984
ishandahal <ishan.dahal88@gmail.com>
2025-08-28 12:54:26 -0400

    Add training loop with logging

--- cs336_basics/train.py
+++ cs336_basics/train.py
@@ -76,3 +76,120 @@ def get_tokenizer():
     merges_filepath = "data/bpe_tiny_stories_merges_10k.pkl"
     special_tokens = ["<|endoftext|>"]
     return Tokenizer.from_files(vocab_filepath, merges_filepath, special_tokens)
+
+
+def train(args):
+    # run = wandb.init(
+    #     project="gpt-2-run-layer-norm-ablation-v0.0",  # Specify project
+    #     config=args,  # Track hyperparameters and metadata
+    # )
+
+    model = get_model(args)
+    model = torch.compile(model, backend="aot_eager")  # For macbook
+    tokenizer = get_tokenizer()
+
+    optimizer = AdamW(
+        model.parameters(),
+        lr=args.learning_rate_max,
+        weight_decay=args.weight_decay,
+    )
+
+    print("Initiating model training")
+    model.train()
+    # Accumulate training loss
+    running_loss = 0
+    prompt = "Once upon a time"
+    generated_samples = []
+    warmup_iters = int(0.1 * args.steps)
+    for i in range(args.steps):
+
+        # Calculate current learning rate
+        current_lr = learning_rate_scheduler(
+            i,
+            lr_max=args.learning_rate_max,
+            lr_min=args.learning_rate_min,
+            warmup_iters=warmup_iters,
+            cosine_annealing_iters=args.steps,
+        )
+        # Update all parameter group
+        for param_group in optimizer.param_groups:
+            param_group["lr"] = current_lr
+
+        start = time.time()  # To track one batch
+        i = i + 1  # For easier iteration logging
+        x, y = get_batch(
+            args.train_path,
+            args.batch_size,
+            args.context_length,
+            deterministic_sampling=None,
+            device=args.device,
+        )
+
+        # forward pass
+        logits = model(x)
+        loss = cross_entropy(logits, y)
+
+        # Backward pass
+        loss.backward()
+
+        # If the norm of the gradients are too large, adjust
+        gradient_clipping(model.parameters(), max_norm=1.0)
+        optimizer.step()
+
+        # Zero gradients
+        optimizer.zero_grad()
+
+        end = time.time()  # To track one batch training
+
+        # Accumulate loss
+        running_loss += loss.item()
+        if not (i % 100):
+            model.eval()
+            average_val_loss = evaluate_validation(model, args)
+            perplexity = torch.exp(average_val_loss)
+
+            print(
+                f"iteration: {i} | training running loss: {running_loss / (i)} | average val loss: {average_val_loss} | average perplexity: {perplexity}"
+            )
+            print(f"Time elapsed for one training step: {elapsed_seconds(start, end)}")
+            if not (i % 500):
+                print("\tGenerating some text: ")
+                response = decoding(
+                    tokenizer,
+                    prompt,
+                    context_length=args.context_length,
+                    max_length=args.max_length,
+                    model=model,
+                    temp=args.temp,
+                    top_p=args.top_p,
+                    device=args.device,
+                )
+                # Collect generated samples
+                generated_samples.append(response)
+            # Logging loss to weights and biases
+            # run.log(
+            #     {
+            #         "train_loss": running_loss / i,
+            #         "validation_loss": average_val_loss,
+            #         "validation_perplexity": perplexity,
+            #     },
+            #     step=i,
+            # )
+
+        # Serialize model and optimizer states
+        # if not (i % 1000):
+        #     print(f"check pointing model at iteration: {i}")
+        #     save_checkpoint(
+        #         model, optimizer, (i - 1), args.checkpoint_path
+        #     )  # i is subtraction by 1 above for ease
+
+    # Weights and bias specific logging
+    # table = wandb.Table(["step", "prompt", "output"])
+    # for j, (prompt, response) in enumerate(
+    #     zip((prompt for _ in range(len(generated_samples))), generated_samples)
+    # ):
+    #     table.add_data(j, prompt, response)
+    #
+    # wandb.log({"generated text samples": table})
+    # run.finish()
+    print("Training complete")
