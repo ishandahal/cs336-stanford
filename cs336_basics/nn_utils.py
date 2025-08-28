a148823c77d2cf570b2e437242a0bced85d77f70
ishandahal <ishan.dahal88@gmail.com>
2025-08-28 11:14:37 -0400

    Add gradient clipping

--- cs336_basics/nn_utils.py
+++ cs336_basics/nn_utils.py
@@ -1,3 +1,8 @@
+import math
+import random
+from typing import Iterable
+
+import numpy as np
 import torch
 
 
@@ -21,3 +26,37 @@ def cross_entropy(logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
     loss = logits_of_targets - denom  # log of division is a subtraction
     neg_loss = -loss
     return neg_loss.mean()
+
+
+def learning_rate_scheduler(
+    iter: int,
+    lr_max: float,
+    lr_min: float,
+    warmup_iters: int,
+    cosine_annealing_iters: int,
+) -> float:
+    if iter <= warmup_iters:
+        return (iter / warmup_iters) * lr_max
+    if warmup_iters < iter < cosine_annealing_iters:
+        cosine_part = 1 + math.cos(
+            ((iter - warmup_iters) / (cosine_annealing_iters - warmup_iters)) * math.pi
+        )
+        return lr_min + ((0.5 * cosine_part) * (lr_max - lr_min))
+
+    return lr_min
+
+
+def gradient_clipping(
+    parameter_list: Iterable[torch.nn.Parameter], max_norm: float, eps: float = 1e-6
+) -> None:
+    accumulate = 0
+    for parameter in parameter_list:
+        if parameter.requires_grad:
+            accumulate += (parameter.grad**2).sum()
+    l2_norm = math.sqrt(accumulate)
+
+    if l2_norm < max_norm:
+        return
+    for parameter in parameter_list:
+        if parameter.requires_grad:
+            parameter.grad *= max_norm / (l2_norm + eps)
